4660bb53077e720049da72934b150bc1a18b9e9f
ducksauce007 <ms1279@gmail.com>
2015-04-11 00:05:35 +0400

    double tap and sd card
    need to recheck sd card though

--- lcvdopenGLold/src/com/example/lcvdopenglold/MainActivity.java
+++ lcvdopenGLold/src/com/example/lcvdopenglold/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.lcvdopenglold;
 
+import java.io.File;
+
 import javax.microedition.khronos.opengles.GL10;
 
 import com.twicecircled.spritebatcher.Drawer;
 import com.twicecircled.spritebatcher.SpriteBatcher;
 
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.ConfigurationInfo;
 import android.graphics.Rect;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements Drawer{
 
@@ -24,6 +26,16 @@ public class MainActivity extends Activity implements Drawer{
 	//Rect dst1 = new Rect(0,0,128,128);
 	float timenow,deltatime,timelast;
 	int image_num = 0;
+	boolean pause = true;
+	int[] resourceIds;
+	
+    TwoFingerDoubleTapListener multiTouchListener = new TwoFingerDoubleTapListener() {
+        @Override
+        public void onTwoFingerDoubleTap() {
+            Toast.makeText(MainActivity.this, "Two Finger Double Tap", Toast.LENGTH_SHORT).show();
+            pause = !pause;
+        }
+    };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +50,15 @@ public class MainActivity extends Activity implements Drawer{
         
         setContentView(glSurf);
         
-        int[] resourceIds = new int[] {R.drawable.test};
+    	File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "/animation_sprite");
+
+    	if(path.exists() && path.isDirectory()) {
+            Toast.makeText(MainActivity.this, "SD card images", Toast.LENGTH_SHORT).show();
+            resourceIds = new int[] {R.string.animation_sprite};
+    	} else
+    		resourceIds = new int[] {R.drawable.test};
+        
+
         
         glSurf.setRenderer(new SpriteBatcher(this, resourceIds, this));
 
@@ -49,35 +69,31 @@ public class MainActivity extends Activity implements Drawer{
 	public void onDrawFrame(GL10 gl, SpriteBatcher sb) {
 		timenow = System.nanoTime()/1000000.0f;  //capture the time once in miliseconds
 	    deltatime = timenow-timelast;
-	    if (image_num == 0){
-	    	sb.draw(R.drawable.test, src1, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num++;
-	    }
-	    else if (image_num == 1 ){
-	    	sb.draw(R.drawable.test, src3, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num++;
-	    }
-	    else if (image_num == 2 ){
-	    	sb.draw(R.drawable.test, src3, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num++;
-	    }
-	    else if (image_num == 3 ){
-	    	sb.draw(R.drawable.test, src2, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num++;
-	    }
-	    else if (image_num == 4 ){
-	    	sb.draw(R.drawable.test, src3, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num++;
-	    }
-	    else {
-	    	sb.draw(R.drawable.test, src3, new Rect(0,0,sb.getViewWidth(),sb.getViewHeight()));
-	    	image_num = 0;
-	    }
-		
-	    Log.v("FrameTime","Millis between this frame and prev: "+(deltatime));
+	    if (!pause) {
+			if (image_num == 0) {
+				sb.draw(R.drawable.test, src1, new Rect(0, 0,
+						sb.getViewWidth(), sb.getViewHeight()));
+				image_num++;
+			} else if (image_num == 3) {
+				sb.draw(R.drawable.test, src2, new Rect(0, 0,
+						sb.getViewWidth(), sb.getViewHeight()));
+				image_num++;
+			} else {
+				sb.draw(R.drawable.test, src3, new Rect(0, 0,
+						sb.getViewWidth(), sb.getViewHeight()));
+				if (image_num == 4)
+					image_num = 0;
+				else
+					image_num++;
+			}
+		}
+		Log.v("FrameTime","Millis between this frame and prev: "+(deltatime));
 		timelast = timenow;
 		
 	}
-
 	
+    public boolean onTouchEvent(MotionEvent event) {
+      	return multiTouchListener.onTouchEvent(event);
+    }
+    
 }
