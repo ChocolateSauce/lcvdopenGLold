8aee7d30ad4d0328d4c42bfce854ca7762ec02d9
cloudsauce <anant.mohan@mymail.aud.edu>
2015-04-13 17:10:36 +0400

    six images shown
    equal length (no black)

--- lcvdopenGLold/gen/com/example/lcvdopenglold/R.java
+++ lcvdopenGLold/gen/com/example/lcvdopenglold/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int test=0x7f020001;
         public static final int test2=0x7f020002;
+        public static final int test_six=0x7f020003;
     }
     public static final class string {
         public static final int animation_sprite=0x7f030001;
